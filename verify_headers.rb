d9e4243f8ab9613a7ff047e65aec21d997cfc115
IN9R <58347338+IN9R@users.noreply.github.com>
2024-07-04 11:40:30 +0200

    Delete verify_headers.rb

--- verify_headers.rb
+++
@@ -1,8 +0,0 @@
-headers_path = "_site/_headers"
-if File.exist?(headers_path)
-  puts "_headers file successfully copied to _site directory."
-  exit 0
-else
-  puts "ERROR: _headers file was not copied to _site directory."
-  exit 1
-end
